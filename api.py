eb2cdd1c22ee9d7fe009352e102313b4d646e909
Miguelmargar <miguelitalia@hotmail.com>
2019-05-20 16:37:39 +0100

    Irish Rail stations api call practice

---
+++ api.py
@@ -0,0 +1,17 @@
+import requests
+import xml.etree.ElementTree as ET
+
+def get_data():
+
+    data = requests.get("http://api.irishrail.ie/realtime/realtime.asmx/getAllStationsXML")
+    root = ET.fromstring(data.content)    
+    
+    j = 0
+    for i in root:
+        st_name = root[j][0].text
+        st_lat = root[j][2].text
+        st_lon = root[j][3].text
+        print(st_name + " is at: " + st_lat + ", " + st_lon)
+        j += 1  
+
+get_data()
