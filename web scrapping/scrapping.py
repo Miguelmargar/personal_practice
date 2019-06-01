8f885de7efa060b2b2a5f5ab1868502619619c71
Miguelmargar <miguelitalia@hotmail.com>
2019-06-01 18:21:41 +0100

    made code a function and added comments to code

--- web scrapping/scrapping.py
+++ web scrapping/scrapping.py
@@ -3,13 +3,28 @@ from bs4 import BeautifulSoup
 
 
 
-data = requests.get("https://finance.yahoo.com/quote/BIRG.IR?p=BIRG.IR")
+def getInfo():
+    """
+    Function to get the share price and percentage movement of the given
+    share - taken from yahoo finance webpage
 
-soup = BeautifulSoup(data.text, "html.parser")
+    """
+    # Company share price ticker
+    company = "BIRG.IR"
 
-span = soup.find_all('span')
+    # yahoo finance page
+    data = requests.get("https://finance.yahoo.com/quote/" + company)
 
-for i in span:
-    if i['data-reactid'] == "14" or i['data-reactid'] == "16":
-        print(i.get_text())
+    # Scrapping 
+    soup = BeautifulSoup(data.text, "html.parser")
+
+    # Get all the spans
+    span = soup.find_all('span')
+
+    # Get the required spans
+    for i in span:
+        if i['data-reactid'] == "14" or i['data-reactid'] == "16":
+            print(i.get_text())
                 
+
+getInfo()
