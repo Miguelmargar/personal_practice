6aa5815b546ac41200b5c775687872c84eacd387
Miguel Martinez <miguelitalia@hotmail.com>
2019-06-03 23:09:21 +0100

    ammended scrapping.py to make it a class rather than just a function

--- web scrapping/scrapping.py
+++ web scrapping/scrapping.py
@@ -3,58 +3,71 @@ from bs4 import BeautifulSoup
 from datetime import *
 import csv
 
+class scraping():
 
-def getInfo():
-    """
-    Function to get the share price, percentage movement of the given
-    share and date of data retrieval to write on file
-    - Share price and percentage taken from yahoo finance webpage
+    def __init__(self):
+        self.write = True
 
-    """
-    # Company share price ticker
-    company = "BIRG.IR"
+    def get_data(self):
+        """
+        Function to get the share price, percentage movement of the given
+        share and date of data retrieval to write on file
+        - Share price and percentage taken from yahoo finance webpage
 
-    # yahoo finance page
-    data = requests.get("https://finance.yahoo.com/quote/" + company)
+        """
+        # Company share price ticker
+        self.company = "BIRG.IR"
 
-    # Scrapping 
-    soup = BeautifulSoup(data.text, "html.parser")
+        # yahoo finance page
+        self.data = requests.get("https://finance.yahoo.com/quote/" + self.company)
 
-    # Get all the spans
-    span = soup.find_all('span')
+        # Scrapping
+        self.soup = BeautifulSoup(self.data.text, "html.parser")
 
-    # Get the required spans
-    for i in span:
-        if i['data-reactid'] == "14":
-            price = i.get_text()
+        # Get all the spans
+        self.span = self.soup.find_all('span')
 
-        elif i['data-reactid'] == "16":
-            percentage = i.get_text()
+        # Get the required spans
+        for i in self.span:
+            if i['data-reactid'] == "14":
+                self.price = i.get_text()
 
-    # Get the date of the info
-    getDate = datetime.today()     
-    date = str(getDate.day) + "-" + str(getDate.month)+ "-" + str(getDate.year)
+            elif i['data-reactid'] == "16":
+                self.percentage = i.get_text()
+
+        # Get the date of the info
+        self.get_date = datetime.today()
+        self.date = str(self.get_date.day) + "-" + str(self.get_date.month)+ "-" + str(self.get_date.year)
+
 
     # Open and check if the date's info is already in the file
-    write = True
-    with open('prices.csv') as readFile:
-        read_csv = csv.reader(readFile, delimiter=",")
-        check = " " + date
-        for row in read_csv:
-            if check == row[2]:
-                write = False
-    readFile.close()
+    def check_data(self):
+
+        with open('prices.csv') as self.readFile:
+            self.read_csv = csv.reader(self.readFile, delimiter=",")
+            self.check = " " + self.date
+            for row in self.read_csv:
+                if self.check == row[2]:
+                    self.write = False
+        self.readFile.close()
 
     # if the date in question is new add info to csv file else advise
-    if write:
-        file = open('prices.csv', 'a')
-        file.write(price + ", " + percentage + ", " + date + "\n")
-        file.close()
-    else:
-        print("Today's information has already been acted upon")
+    def write_data(self):
+
+        if self.write:
+            self.file = open('prices.csv', 'a')
+            self.file.write(self.price + ", " + self.percentage + ", " + self.date + "\n")
+            self.file.close()
+        else:
+            print("Today's information has already been acted upon")
 
 
-getInfo()
+
+a = scraping()
+a.get_data()
+a.check_data()
+a.write_data()
+
 
 
 
